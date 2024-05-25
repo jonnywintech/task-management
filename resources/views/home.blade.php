1dc2b589d2b2649a8566dc99606c573595fe0f7f
Nikola Stankovic <jonnywintech@gmail.com>
2024-05-25 20:41:05 +0200

    initial commit with some initial configuration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('template.index')
+
+@section('navbar')
+    @include('partials.header')
+@endsection
+
+@section('content')
+
+
+
+@endsection()
+
+@section('footer')
+    @include('partials.footer')
+@endsection
